4d20b905d7c883db2263ebbb4b5753c2826fc292
cher-devops <wabijala@gmail.com>
2025-07-28 10:08:18 -0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,3 @@
-export * from './secure-bucket-stack';
+export * from './secure-bucket';        // If this defines the construct
+export * from './secure-bucket-stack';  // If this defines the CDK stack
+
