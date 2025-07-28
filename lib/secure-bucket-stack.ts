c72014599df14fa572e68e723bd03799ded42e9b
cher-devops <wabijala@gmail.com>
2025-07-28 02:12:37 -0400

    Update secure-bucket-stack.ts

--- lib/secure-bucket-stack.ts
+++ lib/secure-bucket-stack.ts
@@ -1,1 +1,11 @@
-export { SecureBucketStack };
+import { Stack, StackProps } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import { SecureBucket, SecureBucketProps } from './secure-bucket';
+
+export class SecureBucketStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps & { bucketProps?: SecureBucketProps }) {
+    super(scope, id, props);
+
+    new SecureBucket(this, 'MySecureBucket', props?.bucketProps || {});
+  }
+}
