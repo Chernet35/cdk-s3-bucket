0dc56fc03d28eeabd81254407b50c99ed89abc3f
cher-devops <wabijala@gmail.com>
2025-07-28 02:47:50 -0400

    Update secure-bucket-stack.ts

--- lib/secure-bucket-stack.ts
+++ lib/secure-bucket-stack.ts
@@ -1,22 +1,11 @@
-it('creates a bucket without encryption and versioning', () => {
-  const app = new App();
-  const stack = new SecureBucketStack(app, 'TestStackWithoutProps', {
-    bucketProps: {
-      encryption: false,
-      versioning: false,
-    },
-  });
+import { Stack, StackProps } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import { SecureBucket, SecureBucketProps } from './secure-bucket';
 
-  const template = Template.fromStack(stack);
+export class SecureBucketStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps & { bucketProps?: SecureBucketProps }) {
+    super(scope, id, props);
 
-  template.hasResourceProperties('AWS::S3::Bucket', {
-    VersioningConfiguration: {
-      Status: 'Suspended',
-    },
-  });
-
-  // Ensure encryption is NOT present
-  const resources = template.findResources('AWS::S3::Bucket');
-  const bucket = Object.values(resources)[0];
-  expect(bucket.Properties).not.toHaveProperty('BucketEncryption');
-});
+    new SecureBucket(this, 'MySecureBucket', props?.bucketProps);
+  }
+}
