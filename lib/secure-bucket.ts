75d21d11f1c1972d392e15eac2bc9ccdf95fee4b
cher-devops <wabijala@gmail.com>
2025-07-28 00:10:12 -0400

    Update secure-bucket.ts

--- lib/secure-bucket.ts
+++ lib/secure-bucket.ts
@@ -264,10 +264,6 @@ export class SecureBucket extends Construct {
               storageClass: s3.StorageClass.INFREQUENT_ACCESS,
               transitionAfter: Duration.days(30),
             },
-            {
-              storageClass: s3.StorageClass.GLACIER,
-              transitionAfter: Duration.days(90),
-            },
           ],
         },
       ];
