e2d5c74ae7b17f91718e424d856379ad21830ce8
cher-devops <wabijala@gmail.com>
2025-07-28 03:20:16 -0400

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,16 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  roots: ['<rootDir>/test'],
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  testMatch: ['**/?(*.)+(spec|test).ts'],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.{ts,js}',
+    '!src/index.ts', // optional: exclude aggregator file
+  ],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov'],
   coverageThreshold: {
     global: {
       statements: 90,
@@ -9,5 +19,8 @@ module.exports = {
       functions: 90,
       lines: 90,
     },
+  },
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
   },
 };
