d9a3e5a88a414ea3ed6236d0ab9d4486bbb28011
cher-devops <wabijala@gmail.com>
2025-07-28 10:20:43 -0400

    Update secure-bucket.ts

--- src/secure-bucket.ts
+++ src/secure-bucket.ts
@@ -1,3 +1,12 @@
-export class SecureBucket { ... }
-// or
-export default class SecureBucket { ... }
+import { Bucket, BucketEncryption, BucketProps } from 'aws-cdk-lib/aws-s3';
+import { Construct } from 'constructs';
+
+export class SecureBucket extends Bucket {
+  constructor(scope: Construct, id: string, props?: BucketProps) {
+    super(scope, id, {
+      encryption: props?.encryption ?? BucketEncryption.S3_MANAGED,
+      versioned: props?.versioned ?? true,
+      ...props,
+    });
+  }
+}
