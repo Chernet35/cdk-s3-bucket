3f278771f5e746c65ed67fca22fa4ef64469382d
cher-devops <wabijala@gmail.com>
2025-07-28 02:58:20 -0400

    Update index.ts

--- lib/index.ts
+++ lib/index.ts
@@ -1,1 +1,11 @@
-export * from './secure-bucket';
+import { SecureBucket, SecureBucketStack } from '../src/index';
+
+describe('Index exports', () => {
+  it('should export SecureBucket', () => {
+    expect(SecureBucket).toBeDefined();
+  });
+
+  it('should export SecureBucketStack', () => {
+    expect(SecureBucketStack).toBeDefined();
+  });
+});
