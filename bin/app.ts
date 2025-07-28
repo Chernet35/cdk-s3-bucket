42b8d49706690b2c8a82a504066786eeff216b15
cher-devops <wabijala@gmail.com>
2025-07-28 02:01:36 -0400

    Update app.ts

--- bin/app.ts
+++ bin/app.ts
@@ -1,2 +1,12 @@
+#!/usr/bin/env node
+import * as cdk from 'aws-cdk-lib';
 import { SecureBucketStack } from '../lib/secure-bucket-stack';
+
+const app = new cdk.App();
+new SecureBucketStack(app, 'SecureBucketStack', {
+  bucketProps: {
+    encryption: true,
+    versioning: true,
+  },
+});
 
